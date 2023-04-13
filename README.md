e3b3beb79ebc219fb06f923fa245f71ae91db837
AdaN6 <a_disney_da@hotmail.com>
2023-04-13 15:23:51 +0200

    used action in new TaskFrom component & v-model

--- README.md
+++ README.md
@@ -21,3 +21,6 @@ use pinia create a store can keep track of any Global state for the application
 # State management solutions
 
 getters function
+
+# short form for vue boilerplate 
+vbase-ns
---
+++ src/components/TaskForm.vue
@@ -0,0 +1,37 @@
+
+
+<template>
+    <form @submit.prevent="handleSubmit">
+        <input 
+            type="text"
+            placeholder="New Task"
+            v-model="newTask"
+        >
+    </form>
+    <button>Add</button>
+</template>
+
+<script>
+import { useTaskStore } from '../stores/TaskStore'
+    export default {
+        setup() {
+            const taskStore = useTaskStore()
+
+            const newTask = ref('')
+
+            const handleSubmit = () => {
+                if (newTask.value.length > 0 ) {
+                    taskStore.addTask({
+                        title: newTask.value,
+                        isFav: false,
+                        id: Math.floor(Math.random() * 1000)
+                    })
+
+                    newTask.value = ''
+                }
+            }
+
+            return {newTask, handleSubmit}
+        }
+    }
+</script>
